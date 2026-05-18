2ed266243f5ab8712eed6a7f5afed3f11e9ecbf2
abhijith <abhijithabs125@gmail.com>
2026-05-18 17:31:38 +0530

    small bugs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect,useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
 import { getProducts, setPage } from '../redux/slices/products'
 import Navbar from '../components/Navbar'
 import Pagination from '../components/Pagination'
